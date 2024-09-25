cf37d440d065649ad0d57f79e9cd24b9aa5a0ce3
Максим Сергеев <sergeevmaks444@gmail.com>
2024-09-25 15:46:11 +0500

    Добавьте файлы проекта.

---
+++ aa/aa.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int number, evenSum = 0, oddSum = 0;
+
+    cout << "Введите числа (введите 0 для завершения):" << endl;
+
+    while (true) 
+    {
+        cin >> number;
+
+        if (number == 0) 
+        {
+            break;
+        }
+
+        if (number % 2 == 0) 
+        {
+            evenSum += number;
+        }
+        else 
+        {
+            oddSum += number;
+        }
+    }
+
+    cout << "Сумма четных чисел: " << evenSum << endl;
+    cout << "Сумма нечетных чисел: " << oddSum << endl;
+
+    return 0;
+}
